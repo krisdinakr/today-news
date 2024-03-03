61a3d62eb302c277af9bcb28d859c3473068c353
Krisdina <krssdina@gmail.com>
2024-03-03 19:23:25 +0700

    add formatdate util

---
+++ src/presentation/utils/utils.ts
@@ -0,0 +1,8 @@
+export const formatDate = (date: string) => {
+  return new Date(date).toLocaleDateString('en-us', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  });
+};
